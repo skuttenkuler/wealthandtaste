9698bc68cbc993c810f771d1e5ad3bc848b1eede
skuttenkuler <sam.kuttenk@gmail.com>
2022-08-02 17:52:56 -0500

    updated cart, store, checkout templates

--- merch/urls.py
+++ merch/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -6,7 +7,7 @@ from . import views
 
 urlpatterns = [
     path('', views.store, name='store'),
-    path('', views.checkout, name='checkout'),
-    path('', views.cart, name='cart')
+    url('/checkout', views.checkout, name='checkout'),
+    url('/cart', views.cart, name='cart')
 ]
 
