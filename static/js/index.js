87f54aa95ea61d7b8d1b029b6b9f2ad093d7b7d2
skuttenkuler <sam.kuttenk@gmail.com>
2022-08-05 13:58:24 -0500

    added js button logic to add to user cart, added checkout template, data rendering

---
+++ static/js/index.js
@@ -0,0 +1,10 @@
+// --- CART --- //
+var updateBtns =document.getElementsByClassName('update-cart');
+
+for(var i = 0; i < updateBtns.length; i++){
+    updateBtns[i].addEventListener('click', function(){
+        var productID = this.dataset.product;
+        var action = this.dataset.action;
+        console.log(productID, action)
+    })
+}
