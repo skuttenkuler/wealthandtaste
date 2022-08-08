3926f46a3b392e473edd09fc3a8a75854d5c3e2d
skuttenkuler <sam.kuttenk@gmail.com>
2022-08-08 09:42:54 -0500

    cookie data issue resolved, .reload() stopping from updating cookie

--- static/js/index.js
+++ static/js/index.js
@@ -1,22 +1,49 @@
 // --- CART --- //
-var updateBtns =document.getElementsByClassName('update-cart');
-
+var updateBtns = document.getElementsByClassName('update-cart');
+//console.log(user)
 for(var i = 0; i < updateBtns.length; i++){
-    updateBtns[i].addEventListener('click', function(){
-        var productID = this.dataset.product;
-        var action = this.dataset.action;
+    updateBtns[i].addEventListener('click',(e)=>{
+        e.preventDefault();
+        //console.log(e.target.dataset)
+        //console.log(data)
+        var productID = e.target.dataset.product;
+        var action = e.target.dataset.action;
         console.log("User: ", user)
         console.log(productID, action)
 
         if(user == 'AnonymousUser'){
-            console.log("not logged in")
+            addCookieItem(productID, action)
         }
         else{
             updateCustomerOrder(productID, action)
         }
     })
 }
+//user not logged in update cookie cart
+function addCookieItem(productID, action){
+    console.log("not logged in");
+    if(action == 'add'){
+        if(cart[productID] == undefined){
+            cart[productID] = {'quantity':1};
+        }else{
+            cart[productID]['quantity'] += 1;
+        }
+    }
+    if(action == 'remove'){
+        cart[productID]['quantity'] - 1;
 
+        if(cart[productID]['quantity'] <= 0){
+            console.log("removed Item")
+            delete cart[productID]
+        }
+    }
+
+    //set cookie
+    console.log('CART:', cart)
+	document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/"
+	
+	//location.reload()
+}
 function updateCustomerOrder(productID, action){
 
     var url = 'update_item/';
@@ -36,7 +63,8 @@ function updateCustomerOrder(productID, action){
     }).then((data) =>{
         console.log(data)
         // once updated reload page
-        location.reload()
+        //location.reload()
     })
 
 }
+
