c125ac362564d2e0cb37750cf9c3b05e18cddf11
skuttenkuler <sam.kuttenk@gmail.com>
2022-12-04 17:25:39 -0600

    initial production build for client launch

--- static/js/index.js
+++ static/js/index.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
         centerMode: false,
         infinite:false,
         arrows: true,
-        prevArrow: '<button class="slide-arrow prev-arrow"></button>',
-        nextArrow: '<button class="slide-arrow next-arrow"></button>',
+        prevArrow: '<button class="slide-arrow prev-arrow"><svg class="slideshow-arrow" width="23" height="39" viewBox="0 0 23 39" style="transform: scaleX(-1) scale(1);"><path d="M857.005,231.479L858.5,230l18.124,18-18.127,18-1.49-1.48L873.638,248Z" fill=var(--primary) transform="translate(-855 -230)"></path></svg></button>',
+        nextArrow: '<button class="slide-arrow next-arrow"><svg style="transform:scaleX(1) scale(1);fill:" width="23" height="39" viewBox="0 0 23 39"><path d="M857.005,231.479L858.5,230l18.124,18-18.127,18-1.49-1.48L873.638,248Z" fill=var(--primary) transform="translate(-855 -230)"></path></svg></button>',
         centerPadding: '60px',
         slidesToShow: 3,
         adaptiveHeight:true,
@@ -15,8 +15,8 @@ $(document).ready(function(){
             breakpoint: 768,
             settings: {
                 arrows: true,
-                prevArrow: '<button class="slide-arrow prev-arrow"></button>',
-                nextArrow: '<button class="slide-arrow next-arrow"></button>',
+                prevArrow: '<button class="slide-arrow prev-arrow"><svg class="slideshow-arrow" width="23" height="39" viewBox="0 0 23 39" style="transform: scaleX(-1) scale(1);"><path d="M857.005,231.479L858.5,230l18.124,18-18.127,18-1.49-1.48L873.638,248Z" fill=var(--primary) transform="translate(-855 -230)"></path></svg></button>',
+                nextArrow: '<button class="slide-arrow next-arrow"><svg style="transform:scaleX(1) scale(1);fill:" width="23" height="39" viewBox="0 0 23 39"><path d="M857.005,231.479L858.5,230l18.124,18-18.127,18-1.49-1.48L873.638,248Z" fill=var(--primary) transform="translate(-855 -230)"></path></svg></button>',
                 centerMode: false,
                 centerPadding: '40px',
                 slidesToShow: 1,
@@ -26,11 +26,13 @@ $(document).ready(function(){
             {
             breakpoint: 480,
             settings: {
-                arrows: true,
-                centerMode: true,
-                centerPadding: '0px',
-                slidesToShow: 1,
-                adaptiveHeight:true,
+              arrows: true,
+              prevArrow: '<button class="slide-arrow prev-arrow"><svg class="slideshow-arrow" width="23" height="39" viewBox="0 0 23 39" style="transform: scaleX(-1) scale(1);"><path d="M857.005,231.479L858.5,230l18.124,18-18.127,18-1.49-1.48L873.638,248Z" fill=var(--primary) transform="translate(-855 -230)"></path></svg></button>',
+              nextArrow: '<button class="slide-arrow next-arrow"><svg style="transform:scaleX(1) scale(1);fill:" width="23" height="39" viewBox="0 0 23 39"><path d="M857.005,231.479L858.5,230l18.124,18-18.127,18-1.49-1.48L873.638,248Z" fill=var(--primary) transform="translate(-855 -230)"></path></svg></button>',
+              centerMode: true,
+              centerPadding: '0px',
+              slidesToShow: 1,
+              adaptiveHeight:true,
             }
             }
         ]
