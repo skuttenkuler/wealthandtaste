1d0844de56ed2b0eff3c30c3d928abb1714bdaf9
skuttenkuler <sam.kuttenk@gmail.com>
2022-12-10 14:50:54 -0600

    uped character amount for description

--- booking/forms.py
+++ booking/forms.py
@@ -4,5 +4,5 @@ from django.forms import ModelForm
 class ClientForm(forms.Form):
     name = forms.CharField(label="Name", max_length=100)
     email = forms.EmailField(label="Email", max_length=100)
-    description = forms.CharField(widget=forms.Textarea,label="Description", max_length=100)
+    description = forms.CharField(widget=forms.Textarea,label="Description", max_length=2000)
     reference = forms.ImageField()
